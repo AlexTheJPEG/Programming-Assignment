54bbb8186aec6241ab0c56a12f4063c29b747e13
Alex The JPEG <alexthejpeg@proton.me>
2024-09-01 22:51:04 -0400

    Draw rects for every clickable node

--- parse.py
+++ parse.py
@@ -1,10 +1,11 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import glob
 import re
 
 pattern = r"bounds=\"(\[(\d+),(\d+)\])(\[(\d+),(\d+)\])\""
 
 for xml_file in glob.glob("Programming-Assignment-Data/*.xml"):
+    name = xml_file[:-4]
     print(xml_file)
     rects = []
     with open(xml_file, "r") as file:
@@ -16,8 +17,16 @@ for xml_file in glob.glob("Programming-Assignment-Data/*.xml"):
             # Go through each line and find a clickable node
             if "clickable=\"true\"" not in line:
                 continue
+            # Use regular expression to get rectangle coordinates of clickable boundary box
             matches = re.findall(pattern, line)[0]
             x1, y1, x2, y2 = int(matches[1]), int(matches[2]), int(matches[4]), int(matches[5])
             rects.append((x1, y1, x2, y2))
     print(rects)
-    print()
+    # Open image counterpart
+    img_file = name + ".png"
+    image = Image.open(img_file).convert("RGBA")
+    draw = ImageDraw.Draw(image)
+    # Highlight each clickable area and save as a new image
+    for rect in rects:
+        draw.rectangle(rect, outline="yellow", width=8)
+    image.save(f"results/{name[28:]} new.png", "PNG")
